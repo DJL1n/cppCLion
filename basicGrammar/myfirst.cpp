36e5a646c24e0ff7d02c202cece7ac4c3a0ffdd2
DJL1n <djl1n_private@163.com>
2024-07-13 17:36:19 +0800

    2024.7.13

---
+++ basicGrammar/myfirst.cpp
@@ -0,0 +1,11 @@
+//
+// Created by legionb on 2024/7/13.
+//
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
