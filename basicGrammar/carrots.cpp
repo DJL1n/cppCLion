63f9f4a52115feb009b9b5b5f819d3904a31b852
DJL1n <djl1n_private@163.com>
2024-07-13 18:25:09 +0800

    2024.7.13

---
+++ basicGrammar/carrots.cpp
@@ -0,0 +1,17 @@
+//
+// Created by legionb on 2024/7/13.
+//
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int carrots;
+    carrots=25;
+    cout<<"I have";
+    cout << carrots;
+    cout << "carrots." << endl;
+    carrots-=1;
+    cout<<"Crunch, crunch. Now I have "<<carrots<<" carrots."<<endl;
+    return 0;
+}
