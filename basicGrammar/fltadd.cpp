29c3c4ce6562c0153c3ddfab9e2069e7d4d5d832
DJL1n <djl1n_private@163.com>
2024-07-19 22:43:30 +0800

    2024.7.19

---
+++ basicGrammar/fltadd.cpp
@@ -0,0 +1,12 @@
+//
+// Created by legionb on 2024/7/18.
+//
+#include<iostream>
+int main() {
+    using namespace std;
+    float a=2.34E+22f;
+    float b=a+1.0f;
+    cout<<"a= "<<a<<endl;
+    cout<<"b-a="<<b-a<<endl;
+    return 0;
+}
