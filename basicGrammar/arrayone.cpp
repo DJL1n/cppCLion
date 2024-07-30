032a3782d9db9a630aec1612aff70f303c2d9908
DJL1n <djl1n_private@163.com>
2024-07-30 18:50:03 +0800

    2024.7.22

---
+++ basicGrammar/arrayone.cpp
@@ -0,0 +1,17 @@
+//
+// Created by legionb on 2024/7/22.
+//
+#include <iostream>
+int main() {
+    using namespace std;
+    int yams[3];
+    yams[0]=7;
+    yams[1]=8;
+    yams[2]=6;
+    int a[]={888,777};
+
+    int yamscosts[3]={20,30,5};
+    cout<<"Total yams= ";
+    cout<<yams[0]+yams[1]+yams[2]<<endl;
+    cout<<a[0]<<endl;
+}
