c7f479d61c50a054393017ed3c67b36e3a2054f7
DJL1n <145757864+DJL1n@users.noreply.github.com>
2024-09-03 07:31:37 +0000

    2024.9.3

--- basicGrammar/equal.cpp
+++ basicGrammar/equal.cpp
@@ -4,9 +4,9 @@ int main(){
     int quizscores[10]={20,20,20,20,20,19,20,18,20,20};
     cout<<"Doing it right:\n";
     int i;
-    for (i=0;quizscores[i]=20;i++){
+    for (i=0;quizscores[i]==20;i++)
         cout<<"quiz "<<i<<" is a 20\n";
-    }
+    
     cout<<"Doing it dangerously wrong:\n";
     for (i=0;quizscores[i]=20;i++)
         cout<<"quiz "<<i<<" is a 20\n";
