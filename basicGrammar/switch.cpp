5ba72a39dd2ef612b5c963fe7c6a88169b7d997e
DJL1n <djl1n_private@163.com>
2024-11-15 22:35:04 +0800

    2024.11.15

---
+++ basicGrammar/switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void showmenu();
+void report();
+void comfort();
+int main(){
+    showmenu();
+    int choice;
+    cin>>choice;
+    while (choice!=5){
+        switch(choice){
+            case 1: cout<<"\a\n";
+                    break;
+
+        }
+    }
+
+}
