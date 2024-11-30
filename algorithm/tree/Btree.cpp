812600cd5150da95870d6cc8669a7b53b99ec5f3
Djl1n <djl1n_private@163.com>
2024-11-30 11:00:23 +0800

    11.30 11h

---
+++ algorithm/tree/Btree.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef struct Bnode{
+    char data;
+    Bnode *lchild,*rchild;
+}Bnode, *Btree;
+
+//创建二叉树
+void createTree(Btree &T){
+    char check;
+    T=new Bnode;
+    cout<<"请输入节点信息："<<endl;
+    cin>>T->data;
+    cout<<"是否添加"<<T->data<<"的左孩子？（Y/N）"<<endl;
+    cin>>check;
+    if (check=='Y')
+        createTree(T->lchild);
+    else
+        T->lchild=NULL;
+        cout<<"是否添加"<<T->data<<"右孩子？（Y/N）"<<endl;
+        cin>>check;
+        if (check=='Y')
+            createTree(T->rchild);
+        else
+            T->rchild=NULL;
+    
+}
+
+void preorder(Btree T){//先序遍历
+    if (T){
+        cout<<T->data<<" ";
+        preorder(T->lchild);
+        preorder(T->rchild);
+    }
+}
+
+void inorder(Btree T){//中序遍历
+    if (T){
+        inorder(T->lchild);
+        cout<<T->data<<" ";
+        inorder(T->rchild);
+    }
+}
+
+void posorder(Btree T){//后续遍历
+    if (T){
+        posorder(T->lchild);
+        posorder(T->rchild);
+        cout<<T->data<<" ";
+    }
+}
+
