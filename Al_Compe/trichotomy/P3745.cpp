89e7cd2a24d7c55a4316d790c6f7020d9d0299bc
Djl1n <djl1n_private@163.com>
2025-03-04 20:58:24 +0800

    2025.3.4

--- Al_Compe/trichotomy/P3745.cpp
+++ Al_Compe/trichotomy/P3745.cpp
@@ -10,5 +10,24 @@ ll calc1(int p){
         if (b[i]<p) x+=p-b[i];
         else    y+=b[i]-p;
     }
-    
+    if (A<B)    return min(x,y)*A+(y-min(x,y))*B;
+    else return y*B;
+}
+
+ll calc2(int p){
+    ll sum=0;
+    for (int i=1;i<=n;i++)
+        if (t[i]<p) sum +=(p-t[i])*C;
+}
+
+int main(){
+    cin>>A>>B>>C>>n>>m;
+    for (int i=1;i<=n;i++)  cin>>t[i];
+    for (int i=1;i<=m;i++)  cin>>b[i];
+    sort(b+1,b+m+1);    sort(t+1,t+n+1);
+    if (C>=1e16){cout<<calc1(t[i])<<end;return 0;}
+    ans=1e16;
+    int l=1,r=N;
+    while (r-1>2)
+    return 0;
 }
